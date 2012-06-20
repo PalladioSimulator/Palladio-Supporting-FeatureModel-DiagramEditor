aeded8ce890890db928fa7b3db13614affecb068
BenjaminKlatt <klatt@fzi.de>
2012-06-20 10:24:10 +0000

    renamed package
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/FeatureModel/EMFFeatureModel/trunk@15454 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.eclipse.featuremodel.diagrameditor/src/org/eclipse/featuremodel/treeeditor/utilities/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * The package of utilities.
- * The classes in this package provides common utilities used throughout all classes.
- * 
- * @author Alexander Moor
- */
-package org.eclipse.featuremodel.treeeditor.utilities;
-
